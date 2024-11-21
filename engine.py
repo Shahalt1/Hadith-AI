635965da97c635042d07ff3db7faaae38e3e74b0
Shahal Thayyil <shahalthayyil2020@gmail.com>
2024-11-21 14:11:53 +0530

    MVP developed

--- engine.py
+++ engine.py
@@ -4,23 +4,67 @@ from langchain_core.documents import Document
 from helper_utilities import get_contents, fetch_hadith_collection, get_category_links
 import re
 from tqdm import tqdm
-
-documents = []
-links, book_titles = fetch_hadith_collection('https://hadithcollection.com/sahihbukhari')
-for link, (book_title, total_pages) in tqdm(zip(links, book_titles), desc='Outer loop'):
-    for page in tqdm(range(1, total_pages + 1), desc="Inner loop"):
-        urls = get_category_links(f"{link}/page/{page}")
-        for url in urls:
-            content, title = get_contents(url)
-            documents.append(Document(page_content=content, metadata={"title": title, "url": url, "category": book_title}))
+from chromadb import PersistentClient
+from chromadb.utils import embedding_functions
+import pandas as pd
 
 
+def get_hadith_collection(url):
+    documents = []
+    links, book_titles = fetch_hadith_collection(url)
+    for link, (book_title, total_pages) in tqdm(zip(links, book_titles), desc='Outer loop'):
+        for page in tqdm(range(1, total_pages + 1), desc="Inner loop"):
+            urls = get_category_links(f"{link}/page/{page}")
+            for url in urls:
+                content, title = get_contents(url)
+                documents.append(Document(page_content=content, metadata={"title": title, "url": url, "category": book_title}))
 
-print(len(documents))
 
 
-with open("dataset.txt", 'w', encoding="utf8") as file:
+
+def create_dataset(documents):
+    with open("dataset.txt", 'w', encoding="utf8") as file:
+        for doc in documents:
+            num = doc.metadata['title'].split('Number')[1]
+            number = re.findall(r'\d+', num)[0]
+            file.write(f"{int(number)}\n{doc.metadata['title']}\n{doc.metadata['category']}\n{doc.metadata['url']}\n{doc.page_content}\n\n\n")
+            
+import pandas as pd
+def create_csv_from_documents(documents, filename="output.csv"):
+    """Creates a CSV file from a list of Langchain Documents."""
+
+    data = []
     for doc in documents:
-        num = doc.metadata['title'].split('Number')[1]
-        number = re.findall(r'\d+', num)[0]
-        file.write(f"{int(number)}\n{doc.metadata['title']}\n{doc.metadata['category']}\n{doc.metadata['url']}\n{doc.page_content}\n\n\n")
+        row = {
+            "page_content": doc.page_content,
+            **doc.metadata
+        }
+        data.append(row)
+
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+    
+    
+# documents =  get_hadith_collection('https://hadithcollection.com/sahihbukhari')
+# create_dataset(documents)
+# create_csv_from_documents(documents)
+
+df = pd.read_csv('output.csv')
+df.reset_index(inplace=True)
+
+client = PersistentClient("vdb", )
+# client.delete_collection(name="hadith-collection")
+collection = client.get_or_create_collection("hadith-collection", metadata={"hnsw:space": "cosine"})
+
+
+def upsert_data(df):
+    for _, row in df.iterrows():
+        collection.upsert(
+            ids=[str(row['index'])],
+            documents=[row['page_content']],
+            metadatas=[
+                {"Title": row['title'], "Category": row['category'], "URL": row['url']}
+            ]
+        )
+
+# upsert_data(df)
---
+++ main.py
@@ -0,0 +1,88 @@
+import streamlit as st
+from google import generativeai as genai
+import time
+import chromadb
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+api_key = getenv("GEMINI_API_KEY")
+collection = chromadb.PersistentClient('vdb').get_collection("hadith-collection")
+
+def stream_data(response):
+    for word in response.split(" "):
+        yield word + " "
+        time.sleep(0.05)
+
+
+def generate_prompt(query):
+        result = results(query)
+        prompt = f"""
+            The Question is {query}
+            summarize the answer using the context : {result['documents'][0][0]} in plain text and formal way
+        """
+        return prompt
+
+def results(query):
+    result = collection.query(
+        query_texts= query, 
+        include=['documents', 'metadatas'],
+        n_results=1
+    )
+    return result
+    
+st.title("Hadith AI")
+
+
+
+model = genai.GenerativeModel('gemini-1.5-flash-8b', generation_config=genai.types.GenerationConfig(
+    temperature=0.1,
+    candidate_count=1,
+    stop_sequences=None,
+    max_output_tokens=1000,
+    response_mime_type='text/plain')
+)
+chat = model.start_chat(history=[])
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if api_key :
+    if prompt := st.chat_input("What is up?"):
+        st.chat_message("user").markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        modified_prompt = generate_prompt(prompt)
+        response = chat.send_message(modified_prompt)
+        with st.chat_message("assistant") :
+            st.write_stream(stream_data(response.text))
+        st.session_state.messages.append({"role": "assistant", "content": response.text})
+else:
+    api_key = st.chat_input("Input API Key", )
+    st.warning("Please Input Your API Key")
+    
+st.sidebar.markdown(
+    "<h2 style='font-size:30px;'>Query Info</h2><hr>", 
+    unsafe_allow_html=True
+)
+
+with st.sidebar:
+    if prompt:
+        result = results(prompt)
+        st.markdown(f"### **User Query**")
+        st.markdown(f"{prompt}")
+
+        st.markdown(f"### **Category of Hadith**")
+        st.markdown(f"{result['metadatas'][0][0]['Category']}")
+
+        st.markdown(f"### **Title of Book**")
+        st.markdown(f"{result['metadatas'][0][0]['Title']}")
+
+        st.markdown(f"### **Link to Website**")
+        st.markdown(f"[{result['metadatas'][0][0]['URL']}]({result['metadatas'][0][0]['URL']})")
+
+
+genai.configure(api_key=api_key)
